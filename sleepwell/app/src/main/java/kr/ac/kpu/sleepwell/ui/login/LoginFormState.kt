eabe6f2c80f49a2200bc2241bbd36183c744af65
DY-Kevin-Kim <kevin359717@gmail.com>
2021-01-25 10:33:21 +0900

    start

---
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package kr.ac.kpu.sleepwell.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
