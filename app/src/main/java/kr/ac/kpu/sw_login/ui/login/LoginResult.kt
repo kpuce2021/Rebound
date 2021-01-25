c898c419a34d0e62f9496bfa2ced9a7cb05727d8
DY-Kevin-Kim <kevin359717@gmail.com>
2021-01-25 10:31:51 +0900

    start

--- app/src/main/java/kr/ac/kpu/sw_login/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package kr.ac.kpu.sw_login.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-        val success: LoggedInUserView? = null,
-        val error: Int? = null
-)
