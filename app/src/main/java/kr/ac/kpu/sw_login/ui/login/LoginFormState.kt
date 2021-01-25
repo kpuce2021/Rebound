e59e0948a1049991ee42adccdd86cc91928dc07f
DY-Kevin-Kim <kevin359717@gmail.com>
2021-01-25 10:25:23 +0900

    login

---
+++ app/src/main/java/kr/ac/kpu/sw_login/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package kr.ac.kpu.sw_login.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
