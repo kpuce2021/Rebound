f7b7822a511fe397f98552c70f6fc9966278cd71
namjoonjo <njoon2ne@gmail.com>
2021-01-29 19:50:03 +0900

    trend add2

---
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Trend_childFrag3_all.kt
@@ -0,0 +1,21 @@
+package kr.ac.kpu.sleepwell
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Trend_childFrag3_all : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trend_child_frag3_all, container, false)
+    }
+
+
+}
