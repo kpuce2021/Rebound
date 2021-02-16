3c11f15ba5e64994ceb47489759f5f6eff27f7c1
DY-Kevin-Kim <kevin359717@gmail.com>
2021-02-16 17:55:58 +0900

    permisson

--- sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Fragment0.kt
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Fragment0.kt
@@ -1,23 +1,31 @@
 package kr.ac.kpu.sleepwell
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_0.view.*
 import java.io.*
 
+
 class Fragment0 : Fragment(), SensorEventListener {
 
 
-    val foldername: String = android.os.Environment.getExternalStorageDirectory().getAbsolutePath().toString() + "/TestLog"
+    val foldername: String = "LogFolder"
     val filename = "sensorlog.txt"
 
 
@@ -27,12 +35,32 @@ class Fragment0 : Fragment(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
+    private fun Permissions(): Boolean {
+        val permissionWRITE_EXTERNAL_STORAGE = activity?.applicationContext?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.WRITE_EXTERNAL_STORAGE) }
+        val permissionREAD_EXTERNAL_STORAGE=activity?.applicationContext?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.READ_EXTERNAL_STORAGE) }
+        val listPermissionsNeeded: MutableList<String> = ArrayList()
+        if (permissionWRITE_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (permissionREAD_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(activity?.applicationContext as Activity, listPermissionsNeeded.toTypedArray(), 100)
+            return false
+        }
+        return true
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         var i = 0
         var view = inflater.inflate(R.layout.fragment_0,container,false)
+        //if(!Permissions())
+            //finish()
         view.sleep_btn.setOnClickListener{
             if(i==0) {
                 sensorManager.registerListener(this,    // 센서 이벤트 값을 받을 리스너 (현재의 액티비티에서 받음)
@@ -51,18 +79,23 @@ class Fragment0 : Fragment(), SensorEventListener {
     }
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
 
+    fun finish(){
+        finish()
+    }
+
     fun WriteTextFile(foldername: String, filename: String, contents: String?) {
         try {
-            val dir = File(foldername)
+            val dir = File("/mnt/sdcard"+File.separator+foldername)
             //디렉토리 폴더가 없으면 생성함
             if (!dir.exists()) {
                 dir.mkdir()
             }
             //파일 output stream 생성
-            val fos = FileOutputStream("$foldername/$filename", true)
+            val fos = FileOutputStream("/mnt/sdcard/$foldername/$filename", true)
             //파일쓰기
             val writer = BufferedWriter(OutputStreamWriter(fos))
             writer.write(contents)
+            Log.d("savepath",foldername)
             writer.flush()
             writer.close()
             fos.close()
