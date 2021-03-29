c479d2d97cf6638bf74c3e49e4ebf6a95e282d9c
DY-Kevin-Kim <kevin359717@gmail.com>
2021-03-29 14:51:52 +0900

    philips hue user id complete

--- sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/HueActivity.kt
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/HueActivity.kt
@@ -5,45 +5,43 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_hue.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Field
+import retrofit2.http.Body
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
-import java.io.IOException
-import java.net.MalformedURLException
-import java.net.URL
-import java.security.SecureRandom
-import java.security.cert.CertificateException
-import java.security.cert.X509Certificate
-import javax.net.ssl.*
+import retrofit2.http.PUT
+import retrofit2.http.GET
 
 
 data class Response(
         @SerializedName("success")
-        var success:List<ResBody>,
+        var success:ResBody,
         @SerializedName("error")
-        var error:List<ResBody>,
+        var error:ResBody,
         @SerializedName("1")
-        var lightid1:List<ResBody>,
+        var lightid1:ResBody,
         @SerializedName("2")
-        var lightid2:List<ResBody>,
+        var lightid2:ResBody,
         @SerializedName("3")
-        var lightid3:List<ResBody>,
+        var lightid3:ResBody,
         @SerializedName("4")
-        var lightid4:List<ResBody>,
+        var lightid4:ResBody,
         @SerializedName("5")
-        var lightid5:List<ResBody>,
+        var lightid5:ResBody,
         @SerializedName("6")
-        var lightid6:List<ResBody>,
+        var lightid6:ResBody,
         @SerializedName("7")
-        var lightid7:List<ResBody>,
+        var lightid7:ResBody,
         @SerializedName("8")
-        var lightid8:List<ResBody>,
+        var lightid8:ResBody,
         @SerializedName("state")
-        var lightstate: List<ResBody>
+        var lightstate: ResBody
 
 )
 
@@ -53,7 +51,7 @@ data class ResBody(
         @SerializedName("username")
         var userid:String,
         @SerializedName("state")
-        var lightstate:List<ResBody>,
+        var lightstate: ResBody?,
         @SerializedName("on")
         var onoff:Boolean,
         @SerializedName("sat")
@@ -61,23 +59,30 @@ data class ResBody(
         @SerializedName("hue")
         var hue : Int,
         @SerializedName("bri")
-        var bri : Int
+        var bri : Int,
+        @SerializedName("type")
+        var code:Int,
+        @SerializedName("address")
+        var address:String
 )
 
 interface HueService{
-    @FormUrlEncoded
     @POST("/api")
     fun getuserid(
-            @Field("devicetype") huenm:String
-    ):Call<Response>
+            @Body request: okhttp3.RequestBody
+    ):Call<List<Response>>
 }
 
 
 class HueActivity : AppCompatActivity() {
-
-    private val bridge = "http://192.168.0.3"
+    //192.168.0.3
+    private val http = "http://"
+    private lateinit var bridge :String
     private lateinit var hueid : String
     val huenm : String = "test"
+    private lateinit var hueurl : String
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,29 +90,35 @@ class HueActivity : AppCompatActivity() {
 
 
         useridBtn.setOnClickListener{
-
-
+            bridge=http+ipET.text.toString()
             val retrofitsetting = Retrofit.Builder()
                     .baseUrl(bridge)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             val api = retrofitsetting.create(HueService::class.java)
+            val newuser =JSONObject()
+            newuser.put("devicetype",huenm)
+            val newuserString = newuser.toString()
+            val userrequest = newuserString.toRequestBody("application/json".toMediaTypeOrNull())
             Runnable {
-                api.getuserid(huenm).enqueue(object : Callback<Response> {
-                    override fun onFailure(call: Call<Response>, t: Throwable) {
+                api.getuserid(userrequest).enqueue(object : Callback<List<Response>> {
+
+                    override fun onFailure(call: Call<List<Response>>, t: Throwable) {
                         Log.d("fail", "failed to get userid : "+ "$t")
                         Log.d("failresponse","$call")
+                        testTV.text = t.toString()
                     }
 
-                    override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                        Log.d("Response:: ", response.body().toString())
-                        Log.d("Res error:: ", response.body()?.error.toString())
-                        Log.d("Res success:: ", response.body()?.success.toString())
-                        val idget: List<ResBody>? = response.body()?.success
+
+                    override fun onResponse(call: Call<List<Response>>, response: retrofit2.Response<List<Response>>) {
+                        Log.d("Response:: ", response.toString())
+                        Log.d("Response body:: ", response.body().toString())
+                        val idget: ResBody? = response.body()?.get(0)?.success
                         if (idget != null) {
-                            hueid = idget[0].userid
+                            hueid = idget.userid
                             Log.d("username", hueid)
                             testTV.text = hueid
+                            hueurl = bridge+"/api/"+hueid
                         }
                     }
                 })
