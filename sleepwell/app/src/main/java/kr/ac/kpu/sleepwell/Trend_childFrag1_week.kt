9303141777e3a28169bf86929de509f211e2669c
namjoonjo <njoon2ne@gmail.com>
2021-03-30 23:06:19 +0900

    트렌드 UI수정

--- sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Trend_childFrag1_week.kt
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Trend_childFrag1_week.kt
@@ -2,21 +2,16 @@ package kr.ac.kpu.sleepwell
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.charts.BarChart
+import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.charts.RadarChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class Trend_childFrag1_week : Fragment() {
@@ -28,156 +23,43 @@ class Trend_childFrag1_week : Fragment() {
         ///////////////////////////////////
         //google fit api로 data받아오는 코드
         ///////////////////////////////////
-        SleepcycleCheck(v)
-        SleepingTimeCheck(v)
-        sleeptimecheck(v)
+        //SleepcycleCheck(v)
+        //SleepingTimeCheck(v)
+        SleepCycleRadar(v)
+        MaxDevibelcheck(v)
         return v
     }
-    fun SleepcycleCheck(v:View){
-        var pieChart: PieChart
-        pieChart=v.findViewById<PieChart>(R.id.piechart)
-        pieChart.setUsePercentValues(true)
-        val entries=ArrayList<PieEntry>()
-        entries.add(PieEntry(508f,"Awake(during Sleep)"))
-        entries.add(PieEntry(600f,"light Sleep"))
-        entries.add(PieEntry(750f,"Deep Sleep"))
-        entries.add(PieEntry(508f,"REM"))
-        entries.add(PieEntry(670f,"Sleep"))
+    fun SleepCycleRadar(v:View){
+        var radarChart: RadarChart
+        radarChart=v.findViewById<RadarChart>(R.id.radar_chart)
 
-        val colorItems=ArrayList<Int>()
-        for(c in ColorTemplate.VORDIPLOM_COLORS) colorItems.add(c)
-        for(c in ColorTemplate.JOYFUL_COLORS) colorItems.add(c)
-        for(c in ColorTemplate.COLORFUL_COLORS) colorItems.add(c)
-        for(c in ColorTemplate.LIBERTY_COLORS) colorItems.add(c)
-        for(c in ColorTemplate.PASTEL_COLORS) colorItems.add(c)
-        colorItems.add(ColorTemplate.getHoloBlue())
+        val data_radar=ArrayList<RadarEntry>()
+        data_radar.add(RadarEntry(8f,15f))
+        data_radar.add(RadarEntry(4f,20f))
+        data_radar.add(RadarEntry(6f,30f))
+        data_radar.add(RadarEntry(9f,20f))
 
-        val pieDataSet= PieDataSet(entries,"")
-        pieDataSet.apply {
-            colors=colorItems
-            valueTextColor= Color.BLACK
-            valueTextSize=16f
-        }
+        var dataset:RadarDataSet=RadarDataSet(data_radar,"SleepCycle")
+        dataset.setColor(Color.BLUE)
 
-        val pieData= PieData(pieDataSet)
-        pieChart.apply {
-            data=pieData
-            description.isEnabled=false
-            isRotationEnabled=false
-            centerText="Sleep Cycle"
-            setEntryLabelColor(Color.BLACK)
-            animateY(1400, Easing.EaseInOutQuad)
-            animate()
-        }
+
+        val data = RadarData()
+        data.addDataSet(dataset)
+        val labels = arrayOf("Awake", "RemSleep", "LightSleep", "DeepSleep")
+        val xAxis = radarChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        xAxis.textColor=R.color.white
+        radarChart.data = data
     }
 
-    fun SleepingTimeCheck(v:View){
-
-        var barChart: BarChart
-        barChart=v.findViewById<BarChart>(R.id.barchart)
-
-        barChart.apply {
-            description.isEnabled=false
-            setMaxVisibleValueCount(7)
-            setPinchZoom(false)
-            setDrawBarShadow(false)
-            setDrawGridBackground(false)
-            setDrawBorders(false)
-            legend.isEnabled=false
-            setTouchEnabled(false)
-            isDoubleTapToZoomEnabled=false
-            animateY(2000)
-        }
-
-        val values=ArrayList<BarEntry>()
-        val type=ArrayList<String>()
-        val colorlist=ArrayList<Int>()
-        var set:BarDataSet
-
-        values.add(BarEntry(1.0f,8.5f))
-        values.add(BarEntry(2.0f,7.1f))
-        values.add(BarEntry(3.0f,10.5f))
-        values.add(BarEntry(4.0f,6.7f))
-        values.add(BarEntry(5.0f,8.6f))
-        values.add(BarEntry(6.0f,8.4f))
-        values.add(BarEntry(7.0f,7.5f))
-
-        type.add("")
-        type.add("Mon")
-        type.add("Tues")
-        type.add("Wedn")
-        type.add("Thurs")
-        type.add("Fri")
-        type.add("Sat")
-        type.add("Sun")
-
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-        colorlist.add(Color.parseColor("#FFFA8072"))
-
-        if(barChart.data!=null && barChart.data.dataSetCount>1){
-            val chartData=barChart.data
-            set=chartData?.getDataSetByIndex(0) as BarDataSet
-            set.values=values
-            chartData.notifyDataChanged()
-            barChart.notifyDataSetChanged()
-        }
-        else{
-            set= BarDataSet(values,"수면시간")
-            set.colors=colorlist
-            set.setDrawValues(false)
-
-            val dataSets=ArrayList<IBarDataSet>()
-            dataSets.add(set)
-
-            val data=BarData(dataSets)
-            barChart.data=data
-            barChart.setVisibleXRange(1.0f,7.0f)
-            barChart.setFitBars(false)
-
-            //x축 설정
-            val xAxis=barChart.xAxis
-            xAxis.apply{
-                setDrawGridLines(false)
-                isEnabled=true
-                position=XAxis.XAxisPosition.BOTTOM
-                setDrawAxisLine(false)
-                granularity=1f
-                isGranularityEnabled=false
-                valueFormatter=IndexAxisValueFormatter(type)
-                textSize=12f
-                textColor= context?.let { ContextCompat.getColor(it,R.color.white) }!!
-            }
-
-            //y축 설정
-            val yAxis=barChart.axisLeft
-            yAxis.apply {
-                setDrawLabels(true)
-                isEnabled = true
-                axisMinimum = 0f // 최소값
-                axisMaximum = 15f // 최대값
-                granularity = 1f // 값 만큼 라인선 설정
-                textColor = Color.RED // 색상 설정
-                axisLineColor = Color.BLACK // 축 색상 설정
-                gridColor = Color.BLUE // 격자 색상 설정
-            }
-            barChart.invalidate()
-        }
-
-    }
-
-    fun sleeptimecheck(v:View){
+    fun MaxDevibelcheck(v:View){
 
         var lineChart: LineChart
-        lineChart=v.findViewById<LineChart>(R.id.linechart)
+        lineChart=v.findViewById<LineChart>(R.id.sound_linechart)
 
         lineChart.apply {
             description.isEnabled=false
-            setMaxVisibleValueCount(8)
+            setMaxVisibleValueCount(16)
             setPinchZoom(false)
             setDrawGridBackground(false)
             setDrawBorders(false)
@@ -185,41 +67,82 @@ class Trend_childFrag1_week : Fragment() {
             setTouchEnabled(false)
             isDoubleTapToZoomEnabled=false
             animateY(2000)
+
+            axisRight.run {
+                isEnabled=false
+                textSize=15f
+            }
         }
 
         val type=ArrayList<String>()
         type.add("")
-        type.add("Mon")
-        type.add("Tues")
-        type.add("Wedn")
-        type.add("Thurs")
-        type.add("Fri")
-        type.add("Sat")
-        type.add("Sun")
+        type.add("12:00")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("7:00")
 
+        val y_type=ArrayList<String>()
+        y_type.add("0db")
+        y_type.add("20db")
+        y_type.add("40db")
+        y_type.add("60db")
+        y_type.add("80db")
+        y_type.add("100db")
+
+        //x축설정
         val xaxis=lineChart.xAxis
         xaxis.apply{
             setDrawGridLines(false)
             isEnabled=true
             position= XAxis.XAxisPosition.BOTTOM
             setDrawAxisLine(false)
-            granularity=1f
+            textColor=R.color.white
+            granularity=0.5f
             isGranularityEnabled=false
             valueFormatter= IndexAxisValueFormatter(type)
             textSize=12f
         }
+        //y축설정
+        val yaxis=lineChart.axisLeft
+        yaxis.apply {
+            isEnabled = true
+            axisMinimum = 0f // 최소값
+            axisMaximum = 100f // 최대값
+            granularity = 10f // 값 만큼 라인선 설정
+            setDrawLabels(true) // 값 셋팅 설정
+            textColor = Color.RED // 색상 설정
+            axisLineColor = Color.BLACK // 축 색상 설정
+            gridColor = Color.BLACK // 격자 색상 설정
+            //valueFormatter=IndexAxisValueFormatter(y_type)
+        }
 
         var entries=ArrayList<Entry>()
 
-        entries.add(Entry(1.0f,3.0f))
-        entries.add(Entry(2.0f,7.0f))
-        entries.add(Entry(3.0f,4.0f))
-        entries.add(Entry(4.0f,9.0f))
-        entries.add(Entry(5.0f,5.0f))
-        entries.add(Entry(6.0f,8.0f))
-        entries.add(Entry(7.0f,7.5f))
+        entries.add(Entry(1.0f,22.0f))
+        entries.add(Entry(2.0f,40.0f))
+        entries.add(Entry(3.0f,21.0f))
+        entries.add(Entry(4.0f,21.0f))
+        entries.add(Entry(5.0f,25.0f))
+        entries.add(Entry(6.0f,28.0f))
+        entries.add(Entry(7.0f,20.5f))
+        entries.add(Entry(8.0f,21.5f))
+        entries.add(Entry(9.0f,21.5f))
+        entries.add(Entry(10.0f,21.5f))
+        entries.add(Entry(11.0f,22.5f))
+        entries.add(Entry(12.0f,21.5f))
+        entries.add(Entry(13.0f,25.5f))
+        entries.add(Entry(14.0f,24.5f))
+        entries.add(Entry(15.0f,26.5f))
+        entries.add(Entry(16.0f,27.5f))
 
         var set1:LineDataSet= LineDataSet(entries,"Dataset1")
+        set1.mode=LineDataSet.Mode.CUBIC_BEZIER
+        set1.setDrawFilled(true)
+        set1.setFillColor(R.color.teal_700)
 
         var datasets=ArrayList<ILineDataSet>()
         datasets.add(set1)
@@ -229,6 +152,6 @@ class Trend_childFrag1_week : Fragment() {
         set1.setColor(Color.BLACK)
         set1.setCircleColor(Color.BLACK)
         lineChart.data=data
-        lineChart.setVisibleXRange(1.0f,7.0f)
+        lineChart.setVisibleXRange(1.0f,16.0f)
     }
 }
--- sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Trend_childFrag3_all.kt
+++ sleepwell/app/src/main/java/kr/ac/kpu/sleepwell/Trend_childFrag3_all.kt
@@ -1,10 +1,17 @@
 package kr.ac.kpu.sleepwell
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 
 class Trend_childFrag3_all : Fragment() {
@@ -12,8 +19,141 @@ class Trend_childFrag3_all : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trend_child_frag3_all, container, false)
+        var v:View=inflater.inflate(R.layout.fragment_trend_child_frag3_all, container, false)
+        ///////////////////////////////////
+        //google fit api로 data받아오는 코드
+        ///////////////////////////////////
+        //SleepcycleCheck(v)
+        //SleepingTimeCheck(v)
+        SleepCycleRadar(v)
+        MaxDevibelcheck(v)
+        return v
+    }
+
+    fun SleepCycleRadar(v:View){
+        var radarChart: RadarChart
+        radarChart=v.findViewById<RadarChart>(R.id.radar_chart)
+
+        val data_radar=ArrayList<RadarEntry>()
+        data_radar.add(RadarEntry(8f,15f))
+        data_radar.add(RadarEntry(4f,20f))
+        data_radar.add(RadarEntry(6f,30f))
+        data_radar.add(RadarEntry(9f,20f))
+
+        var dataset: RadarDataSet = RadarDataSet(data_radar,"SleepCycle")
+        dataset.setColor(Color.BLUE)
+
+
+        val data = RadarData()
+        data.addDataSet(dataset)
+        val labels = arrayOf("Awake", "RemSleep", "LightSleep", "DeepSleep")
+        val xAxis = radarChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        xAxis.textColor=R.color.white
+        radarChart.data = data
+    }
+
+    fun MaxDevibelcheck(v:View){
+
+        var lineChart: LineChart
+        lineChart=v.findViewById<LineChart>(R.id.sound_linechart)
+
+        lineChart.apply {
+            description.isEnabled=false
+            setMaxVisibleValueCount(16)
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+            setDrawBorders(false)
+            legend.isEnabled=false
+            setTouchEnabled(false)
+            isDoubleTapToZoomEnabled=false
+            animateY(2000)
+
+            axisRight.run {
+                isEnabled=false
+                textSize=15f
+            }
+        }
+
+        val type=ArrayList<String>()
+        type.add("")
+        type.add("12:00")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("")
+        type.add("7:00")
+
+        val y_type=ArrayList<String>()
+        y_type.add("0db")
+        y_type.add("20db")
+        y_type.add("40db")
+        y_type.add("60db")
+        y_type.add("80db")
+        y_type.add("100db")
+
+        //x축설정
+        val xaxis=lineChart.xAxis
+        xaxis.apply{
+            setDrawGridLines(false)
+            isEnabled=true
+            position= XAxis.XAxisPosition.BOTTOM
+            setDrawAxisLine(false)
+            textColor=R.color.white
+            granularity=0.5f
+            isGranularityEnabled=false
+            valueFormatter= IndexAxisValueFormatter(type)
+            textSize=12f
+        }
+        //y축설정
+        val yaxis=lineChart.axisLeft
+        yaxis.apply {
+            isEnabled = true
+            axisMinimum = 0f // 최소값
+            axisMaximum = 100f // 최대값
+            granularity = 10f // 값 만큼 라인선 설정
+            setDrawLabels(true) // 값 셋팅 설정
+            textColor = Color.RED // 색상 설정
+            axisLineColor = Color.BLACK // 축 색상 설정
+            gridColor = Color.BLACK // 격자 색상 설정
+            //valueFormatter=IndexAxisValueFormatter(y_type)
+        }
+
+        var entries=ArrayList<Entry>()
+
+        entries.add(Entry(1.0f,22.0f))
+        entries.add(Entry(2.0f,40.0f))
+        entries.add(Entry(3.0f,21.0f))
+        entries.add(Entry(4.0f,21.0f))
+        entries.add(Entry(5.0f,25.0f))
+        entries.add(Entry(6.0f,28.0f))
+        entries.add(Entry(7.0f,20.5f))
+        entries.add(Entry(8.0f,21.5f))
+        entries.add(Entry(9.0f,21.5f))
+        entries.add(Entry(10.0f,21.5f))
+        entries.add(Entry(11.0f,22.5f))
+        entries.add(Entry(12.0f,21.5f))
+        entries.add(Entry(13.0f,25.5f))
+        entries.add(Entry(14.0f,24.5f))
+        entries.add(Entry(15.0f,26.5f))
+        entries.add(Entry(16.0f,27.5f))
+
+        var set1: LineDataSet = LineDataSet(entries,"Dataset1")
+        set1.mode= LineDataSet.Mode.CUBIC_BEZIER
+        set1.setDrawFilled(true)
+        set1.setFillColor(R.color.teal_700)
+
+        var datasets=ArrayList<ILineDataSet>()
+        datasets.add(set1)
+
+        var data: LineData = LineData(datasets)
+
+        set1.setColor(Color.BLACK)
+        set1.setCircleColor(Color.BLACK)
+        lineChart.data=data
+        lineChart.setVisibleXRange(1.0f,16.0f)
     }
 
 }
